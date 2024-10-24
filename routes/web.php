4b375670ec9bacb3962377c3fc9bb4cccfef1f5c
Fabiano Santos Silva <94010474+kFabada@users.noreply.github.com>
2024-10-24 11:52:23 -0300

    controllers auth login, create

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
+
